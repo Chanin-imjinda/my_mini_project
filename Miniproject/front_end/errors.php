3c593d85126779c5a69cb34b551fcab9b8dab8d2
Chanin-imjinda <102347445+Chanin-imjinda@users.noreply.github.com>
2023-10-20 17:30:58 +0700

    Delete Miniproject/front_end directory

--- Miniproject/front_end/errors.php
+++
@@ -1,9 +0,0 @@
-<?php $ereors = array(); ?>
-
-<?php if (count($errors) > 0) : ?>
-    <div class="error">
-        <?php foreach ($erros as $error) : ?>
-            <p><?php echo $error ?></p>
-        <?php endforeach ?>
-    </div>
-<?php endif ?>
